a4e85170010c262a4bae37bfa496679cbb7d2d25
peejur <peejur2750@yahoo.com>
2017-04-05 00:21:03 -0400

    i did shit

---
+++ Assets/FireBow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBow : MonoBehaviour {
+
+	public GameObject arrowspawn;
+	public GameObject arrow;
+	public float arrowforce;
+	public float drawtime = 0.0f;
+	public float MAX;
+	public float temp;
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)){
+			drawtime += Time.deltaTime;
+
+		
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			drawtime += .2f;
+			float force = arrowforce * drawtime;
+			if (force >= MAX)
+				force = MAX;
+			temp = force;//debug purposes
+			GameObject firedarrow = Instantiate(arrow, arrowspawn.transform.position,arrowspawn.transform.rotation);
+			Rigidbody rb = firedarrow.GetComponent<Rigidbody>();
+			rb.AddForce(-arrowspawn.transform.up*force, ForceMode.Impulse);
+			drawtime = 0.0f;
+
+		}
+	}
+}
---
+++ Assets/deadarrow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deadarrow : MonoBehaviour {
+	public float time = 0.0f;
+	void OnCollisionEnter(Collision other){
+		Rigidbody rb = this.GetComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.FreezeAll;
+		rb.useGravity = false;
+		this.GetComponent<SphereCollider> ().enabled = false;
+		this.transform.parent = other.transform.parent;
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
