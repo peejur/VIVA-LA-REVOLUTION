4fea7f46d0cdf0d9af2fe4d82277c9fbfc3700e0
TomMGrello <tommgrello@gmail.com>
2017-04-08 13:29:28 -0400

    Added an explosion effect to explosive arrows. It kinds sucks but meh.

--- Assets/deadarrow.cs
+++ Assets/deadarrow.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class deadarrow : MonoBehaviour {
 	public float time = 0.0f;
+	public GameObject hitEffect;
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "quiver")
 			return;
@@ -12,5 +14,9 @@ public class deadarrow : MonoBehaviour {
 		rb.useGravity = false;
 		this.GetComponent<SphereCollider> ().enabled = false;
 		this.transform.parent = other.transform;
+		if (hitEffect) {
+			Instantiate (hitEffect, transform.position,Quaternion.identity);
+			Destroy (gameObject);
+		}
 	}
 }
